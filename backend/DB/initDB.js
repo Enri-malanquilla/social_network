47512dbf1dcb3e127c839ed630e28433d1971f79
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2023-03-16 12:50:28 +0100

    inicializated initDB

--- backend/DB/initDB.js
+++ backend/DB/initDB.js
@@ -7,7 +7,29 @@ const initDB = async (req, res, next) => {
 
   try {
     connection = await getDB();
-    console.log('aqui esta el init');
+
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP TABLE IF EXISTS photos');
+    await connection.query('DROP TABLE IF EXISTS posts');
+    await connection.query('DROP TABLE IF EXISTS comments');
+    await connection.query('DROP TABLE IF EXISTS likes');
+    await connection.query('DROP TABLE IF EXISTS chat');
+    console.log('TABLES REMOVED');
+
+    //USER'S TABLES
+
+    await connection.query(`
+    CREATE TABLE  users (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(50) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(512) NOT NULL,
+        created_user DATETIME NOT NULL
+    )
+    `);
+
+    console.log('USER TABLE CREATED');
+
   } catch (error) {
     console.error(error.message);
   } finally {
