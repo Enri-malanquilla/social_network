8aabdbdd0edc2a6e88d32fdc7c4e8fe7a15ab2ba
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2023-03-17 09:13:26 +0100

    ini

--- backend/controllers/users/loginUser.js
+++ backend/controllers/users/loginUser.js
@@ -9,20 +9,17 @@ const loginUser = async (req, res, next) => {
 
         const { name, password } = req.body;
 
-        // Si falta algún dato lanzamos un error.
         if (!name || !password) {
             const error = new Error('Faltan campos');
             error.httpStatus = 400;
             throw error;
         }
 
-        // Comprobamos si existe un usuario con ese email y esa contraseña.
         const [user] = await connection.query(
             `SELECT id FROM users WHERE name = ? AND password = SHA2(?, 512)`,
             [name, password]
         );
 
-        // Si el usuario no existe lanzamos un error.
         if (user.length < 1) {
             const error = new Error('Nombre o contraseña incorrectos');
             error.httpStatus = 401;
@@ -30,13 +27,11 @@ const loginUser = async (req, res, next) => {
         }
 
 
-        // Objeto con la información que le queramos pasar al token.
         const tokenInfo = {
             id: user[0].id,
             role: user[0].name,
         };
 
-        // Creamos el token.
         const token = jwt.sign(tokenInfo, process.env.SECRET, {
             expiresIn: '30d',
         });
